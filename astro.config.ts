09227f315c3cad82959b517da820a59d4929f18d
connor <hello@connordavidfitzgerald.com>
2026-02-25 19:58:43 -0500

    v1

--- astro.config.ts
+++ astro.config.ts
@@ -1,4 +1,4 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig, fontProviders } from 'astro/config';
 import icon from 'astro-icon';
 import postcssUtopia from 'postcss-utopia';
 import postcssHelpersFunctions from '@locomotivemtl/postcss-helpers-functions';
@@ -38,21 +38,55 @@ export default defineConfig({
         fonts: [
             {
                 provider: 'local',
-                name: 'Source Sans Pro',
-                cssVariable: '--custom-font-sans',
+                name: 'PP Radio Grotesk',
+                cssVariable: '--radio',
                 fallbacks: ['sans-serif'],
                 variants: [
+                    {
+                        weight: 300,
+                        style: 'normal',
+                        display: 'swap',
+                        src: ['./src/assets/fonts/PPRadioGrotesk-Ultralight.woff']
+                    },
                     {
                         weight: 400,
                         style: 'normal',
                         display: 'swap',
-                        src: ['./src/assets/fonts/SourceSans3-Regular.woff2']
+                        src: ['./src/assets/fonts/PPRadioGrotesk-Regular.woff']
                     },
                     {
                         weight: 700,
                         style: 'normal',
                         display: 'swap',
-                        src: ['./src/assets/fonts/SourceSans3-Bold.woff2']
+                        src: ['./src/assets/fonts/PPRadioGrotesk-Black.woff']
+                    }
+                ]
+            },
+            {
+                provider: 'local',
+                name: 'PP Formula',
+                cssVariable: '--formula',
+                fallbacks: ['sans-serif'],
+                variants: [
+                    {
+                        weight: 700,
+                        style: 'normal',
+                        display: 'swap',
+                        src: ['./src/assets/fonts/PPFormula-CondensedBold.woff']
+                    }
+                ]
+            },
+            {
+                provider: 'local',
+                name: 'PP Editorial New',
+                cssVariable: '--editorial',
+                fallbacks: ['serif'],
+                variants: [
+                    {
+                        weight: 400,
+                        style: 'normal',
+                        display: 'swap',
+                        src: ['./src/assets/fonts/PPEditorialNew-Ultralight.woff']
                     }
                 ]
             }
